e106a08fb5286ff4e90c80858e05337110c830ee
Elyes159 <elyesmlik307@gmail.com>
2024-12-26 16:31:14 +0100

    cds

--- lib/Screens/AuthScreens/SignUpScreen.dart
+++ lib/Screens/AuthScreens/SignUpScreen.dart
@@ -1,11 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutter/material.dart';
 import 'package:finesse_frontend/Screens/AuthScreens/SignIn.dart';
 import 'package:finesse_frontend/Widgets/AuthButtons/CustomButton.dart';
 import 'package:finesse_frontend/Widgets/AuthButtons/SocialMediaSignIn.dart';
 import 'package:finesse_frontend/Widgets/CustomTextField/LoginTextField.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
- // Remplacez par le chemin correct de votre fichier
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -18,194 +16,176 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPassController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  Future<void> _signUp() async {
-    if (_formKey.currentState!.validate()) {
-      try {
-        final FirebaseAuth auth = FirebaseAuth.instance;
-
-        // Créer un nouvel utilisateur
-        UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-          email: _emailController.text.trim(),
-          password: _passwordController.text.trim(),
-        );
-
-        // Envoyer un email de vérification
-        await userCredential.user!.sendEmailVerification();
-
-        // Ajouter des données à Firestore
-        await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
-          'email': _emailController.text.trim(),
-          'createdAt': DateTime.now(),
-        });
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Compte créé avec succès ! Vérifiez votre email.')),
-        );
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Erreur: ${e.toString()}')),
-        );
-      }
-    }
-  }
+  String? _errorMessage;
+ 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          children: [
-            const SizedBox(height: 96),
-            const Text(
-              'Create an account',
-              style: TextStyle(
-                color: Color(0xFF111928),
-                fontSize: 32,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w800,
-                height: 1.38,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 10),
-            const SizedBox(
-              width: 207,
-              height: 48,
-              child: Text(
-                'Fill in information about\nyour account',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color(0xFF111928),
-                  fontSize: 16,
-                  fontFamily: 'Raleway',
-                  fontWeight: FontWeight.w500,
-                  height: 1.38,
-                ),
-              ),
-            ),
-            const SizedBox(height: 24),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  CustomTextFormField(
-                    controller: _emailController,
-                    label: "Your e-mail address",
-                    isPassword: false,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Veuillez entrer votre email.';
-                      }
-                      if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                        return 'Veuillez entrer un email valide.';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  CustomTextFormField(
-                    controller: _passwordController,
-                    label: "Password",
-                    isPassword: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Veuillez entrer un mot de passe.';
-                      }
-                      if (value.length < 6) {
-                        return 'Le mot de passe doit contenir au moins 6 caractères.';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  CustomTextFormField(
-                    controller: _confirmPassController,
-                    label: "Confirm password",
-                    isPassword: true,
-                    validator: (value) {
-                      if (value != _passwordController.text) {
-                        return 'Les mots de passe ne correspondent pas.';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16),
-            CustomButton(
-              onTap: _signUp,
-              label: 'Continue',
-            ),
-            SizedBox(height: 32),
-            Text(
-              'Or use',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF111928),
-                fontSize: 14,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-             SizedBox(height: 14),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Center(
+            child: ListView(
               children: [
-                CustomContainer(
-                  onTap: () {},
-                  imagePath: "assets/Icons/google.svg",
+                const SizedBox(height: 96),
+                const Text(
+                  'Create an account',
+                  style: TextStyle(
+                    color: Color(0xFF111928),
+                    fontSize: 32,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w800,
+                    height: 1.38,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(width: 8.86),
-                CustomContainer(
-                  onTap: () {},
-                  imagePath: "assets/Icons/facebook.svg",
-                ),
-                SizedBox(width: 8.86),
-                CustomContainer(
-                  onTap: () {},
-                  imagePath: "assets/Icons/apple.svg",
-                ),
-              ],
-            ),
-            SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Already have an account?',
+                const SizedBox(height: 10),
+                const Text(
+                  'Fill in information about\nyour account',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Color(0xFF111928),
                     fontSize: 16,
                     fontFamily: 'Raleway',
                     fontWeight: FontWeight.w500,
+                    height: 1.38,
                   ),
                 ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignInScreen()),
-                    );
-                  },
-                  child: Text(
-                    ' Login now',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFFC668AA),
-                      fontSize: 16,
-                      fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w500,
-                    ),
+                const SizedBox(height: 24),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      CustomTextFormField(
+                        controller: _emailController,
+                        label: "Your e-mail address",
+                        isPassword: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email.';
+                          }
+                          if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                            return 'Please enter a valid email.';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      CustomTextFormField(
+                        controller: _passwordController,
+                        label: "Password",
+                        isPassword: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password.';
+                          }
+                          if (value.length < 6) {
+                            return 'Password must be at least 6 characters long.';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      CustomTextFormField(
+                        controller: _confirmPassController,
+                        label: "Confirm password",
+                        isPassword: true,
+                        validator: (value) {
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match.';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      if (_errorMessage != null)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            _errorMessage!,
+                            style: const TextStyle(color: Colors.red, fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      CustomButton(
+                        onTap: (){},
+                        label: 'Continue',
+                      ),
+                    ],
                   ),
+                ),
+                const SizedBox(height: 32),
+                const Text(
+                  'Or use',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFF111928),
+                    fontSize: 14,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 14),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomContainer(
+                      onTap: () {},
+                      imagePath: "assets/Icons/google.svg",
+                    ),
+                    const SizedBox(width: 8.86),
+                    CustomContainer(
+                      onTap: () {},
+                      imagePath: "assets/Icons/facebook.svg",
+                    ),
+                    const SizedBox(width: 8.86),
+                    CustomContainer(
+                      onTap: () {},
+                      imagePath: "assets/Icons/apple.svg",
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Already have an account?',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF111928),
+                        fontSize: 16,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const SignInScreen()),
+                        );
+                      },
+                      child: const Text(
+                        ' Login now',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFFC668AA),
+                          fontSize: 16,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+         
+        ],
       ),
     );
   }
