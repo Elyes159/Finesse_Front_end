814679c73889d6d6545fbfc9081489d7fbe566f8
Elyes159 <elyesmlik307@gmail.com>
2025-01-06 12:52:36 +0100

    csd

--- lib/Screens/SellProduct/SellproductScreen.dart
+++ lib/Screens/SellProduct/SellproductScreen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SellProductScreen extends StatefulWidget {
   const SellProductScreen({super.key});
@@ -8,8 +12,112 @@ class SellProductScreen extends StatefulWidget {
 }
 
 class _SellProductScreenState extends State<SellProductScreen> {
+  final List<File?> _images = List.generate(5, (index) => null); // Liste pour stocker les images
+
+  Future<void> _pickImage(int index) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery, // Ou `ImageSource.camera` pour utiliser la caméra
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _images[index] = File(pickedFile.path); // Met à jour l'image sélectionnée
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            const Text(
+              'Sell item',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w400,
+                height: 1.50,
+                letterSpacing: 0.50,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              width: 343,
+              child: Text(
+                'Add information about items you’re selling to help customers know more about it',
+                style: TextStyle(
+                  color: Color(0xFF334155),
+                  fontSize: 14,
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.w500,
+                  height: 1.43,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            SizedBox(
+              height: 160, // Hauteur définie pour contenir les éléments
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(5, (index) {
+                    return GestureDetector(
+                      onTap: () => _pickImage(index), // Ouvre le sélecteur d'images
+                      child: Container(
+                        width: 160,
+                        height: 160,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: ShapeDecoration(
+                          color: const Color(0xFFE5E7EB),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: _images[index] != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.file(
+                                  _images[index]!,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                            : Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                      "assets/Icons/gallery.svg",
+                                      
+                                    ),
+                                    const SizedBox(height: 12),
+                                    const Text(
+                                      'Add photo',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
